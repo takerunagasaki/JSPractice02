b7f689df9a80ae144b0e9c99797d67cdb837d7a5
EC2 Default User <ec2-user@ip-172-31-6-84.us-east-2.compute.internal>
2021-03-14 01:33:04 +0000

    提出

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
 /* global $ */
 
 let btCnt = 10.0;
+let DotF = 0;
+let NoF = 1;
 let fCnt = 0.0;
+let fCnt1 = 0.0;
 let math = 0.0;
 let math1 = 0.0;
 let math2 = 0.0;
 let mathtext = "";
 let f = 1.0;
-let x = 0.0;
-let xx = 1.0;
+let x;
+let xx;
+let xxx;
+let q;
 
 
-function no(x,xx){
-  if (fCnt == 1.0 && xx == 1.0){  //通常の数字が押されたら
-    f *= 0.1;
-  }else if (fCnt == 1.0 && xx == 10.0){ //00が押されたら。
-    f *= 0.01;
-    xx = 1.0
+function no(x){
+  xx = document.querySelector( '.MathKekka' ).value;
+  
+  if ((((x == '0' || x == '00' ) && xx == "0")) || (x == '.' && DotF == 1)){
+      x = '';
+  }else if((x == "+" || x == "-" || x == "*" || x == "/") && (NoF == 1)){
+      mathtext = ""
+      DotF = 0;
+      NoF = 0;
+  }else if((x == "+" || x == "-" || x == "*" || x == "/" ) && (NoF == 0)){
+      x = '';
+      
+  }else if (x == "." && DotF == 0){
+    NoF = 0;
+    DotF = 1;
+  }else if (xx == "0"){
+      document.querySelector( '.MathKekka' ).value = x;
+      x = '';
+      NoF = 1;
+  }else {
+    mathtext += x;
+    NoF = 1;
   }
-  math = math * (btCnt * xx) + (x * f);
-  $(".MathKekka").text(math); //テキストを変更する処理です。
+  
+  document.querySelector( '.MathKekka' ).value += x;
 
 };
 
-function noPlus(){  //加算
-  
+function noAC(x){  //クリア
+  mathtext = x;
+  DotF = 0;
+  NoF = 1;
+  document.querySelector( '.MathKekka' ).value = x;
+     
 };
-function noMinus(){ //引き算
-  
-};
-function noAst(){ //掛け算
-  
-};
-function noSlash(){ //小数点
-  
-};
-function nodot(){ //ドット
-  fCnt = 1.0;
-  btCnt = 1.0;
-};
-
-function noAC(){  //クリア
-  math = 0.0;
-  fCnt = 0.0;
-  btCnt = 10.0;
-  f = 1;
-  $(".MathKekka").text(math); //テキストを変更する処理です。          
-};
-function noEq(){  //答え
-  
+function noEq(xx){  //答え
+  if (NoF == 1){
+    const v = document.querySelector( '.MathKekka' ).value //インプットに入力された値を取得
+    const q = new Function( 'return ' + v );  　//関数qにvの値を渡す
+    x = (q().toString() )
+    document.querySelector( '.MathKekka').value = x
+  }
 };
